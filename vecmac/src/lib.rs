84aab15b3c0cc9f864243eabc01885f4f7602467
peterblockman <peterblockman.git@protonmail.com>
2022-03-17 13:40:44 +0900

    Complete macro

--- vecmac/src/lib.rs
+++ vecmac/src/lib.rs
@@ -1,33 +1,68 @@
 #[macro_export]
 macro_rules! avec {
-    () => {
-        Vec::new()
-    };
     // double curly braces to say this is a block
     // if we have multiple line and the last line is what we want to return
     // ($(element: expr),+ = 1 ore more expr
     // $(,)? adding an optional trailling comma
-    ($($element: expr),+ $(,)?) => {{
-        let mut vs = Vec::new();
+    // $($element: expr),* to say ) or more repeatation
+    ($($element: expr),* $(,)?) => {{
+        // check that count is const
+        const C: usize =  $crate::count![@COUNT; $($element),*];
+        // allow not pushing when empty
+        #[allow(unused_mut)]
+        let mut vs = Vec::with_capacity(C);
         // repeat what inside the () same number of time as the patter
         // that $element in it. So we can pass as many element as we want
         $(vs.push($element);)*
         vs
     }};
 
+    ($($element:expr),*) => {
+        // $($element),*: 0 or more
+        $crate::avec![$($element),*]
+    };
+
+
     ($element: expr; $count: expr) => {{
-        let mut vs = Vec::new();
+        let count = $count;
+        // let mut vs = Vec::new(): calling new and doing pushes. Whenever we push we create a reallocation of the vector
+        // because Vec have defeault capacity around 16, so after we push 16, it has to reallocate copy of all elements to an Vec that 2x larger than
+        // the origial vector and so on. That is why we use with_capacity
+        let mut vs = Vec::with_capacity(count);
         // define x here and clone above otherwise it does not work with nonliteral type
         // with Option only the fist take will success because there is only 1 interior value
         // we have to clone it everytime we push
-        let x = $element;
-        for _ in 0..$count{
-            vs.push(x.clone());
-        }
+        /*
+            let x = $element;
+            for _ in 0..count{
+                // push
+                vs.push(x.clone());
+            }
+        */
+
+        // when we do push, it has to do pointer increment (bound check)
+
+        // vs.extend(std::iter::repeat($element).take(count));
+
+        // another option is to use resize. This does not do bound check. it is more effecient.
+        // with resize we dont have to use Vec::with_capacity instead we can just use Vec::new()
+        vs.resize(count, $element);
+
         vs
     }}
+}
 
-
+#[macro_export]
+#[doc(hidden)]
+macro_rules!  count{
+    (@COUNT; $($element: expr),*) => {
+        // if we use [()), *] it will throw error
+        // because rust does not know what variable inside
+        // we have to use SUBST take element and return ()
+        // explanation for <[()]> https://stackoverflow.com/a/70407322/10104154
+        <[()]>::len(&[$($crate::count![@SUBST; $element]), *])
+    };
+    (@SUBST; $_element:expr) => { () };
 }
 
 #[test]
